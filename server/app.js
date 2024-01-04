30473732fc4de5e2f4d50e50168c03af15458273
marcoantonetti <marco.a1098@gmail.com>
2024-01-03 22:13:44 -0300

    Changed port

--- server/app.js
+++ server/app.js
@@ -3,28 +3,24 @@ import express, { json } from "express"; // require -> commonJS
 import "dotenv/config";
 import { createTodoRouter } from "./Routes/todoRouter.js";
 import { createUserRouter } from "./Routes/userRouter.js";
-import cors from 'cors'
-import bodyParser from 'body-parser'
+import cors from "cors";
+import bodyParser from "body-parser";
 
 // después
 export const createApp = ({ todoModel, userModel }) => {
   const port = 10000;
   const hostname = "localhost";
   const app = express();
-  console.log(todoModel, {todoModel})
-
+  console.log(todoModel, { todoModel });
 
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
   app.use(cors());
   app.disable("x-powered-by");
-  app.use("/todos",createTodoRouter({todoModel}));
-  app.use("/user",createUserRouter({userModel}));
+  app.use("/todos", createTodoRouter({ todoModel }));
+  app.use("/user", createUserRouter({ userModel }));
 
-
-  app.listen(
-    port,
-    hostname,
-    console.log(`server listening on port http://${hostname}:${port}`)
-  );
+  app.listen(port, () => {
+    console.log(`server listening on port ${port}`);
+  });
 };
