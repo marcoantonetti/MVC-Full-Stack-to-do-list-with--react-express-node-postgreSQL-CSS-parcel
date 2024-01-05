d50388d6b41dbbecc8e00be82c3056bcbe2d7775
marcoantonetti <marco.a1098@gmail.com>
2024-01-05 02:37:05 -0300

    Render connection url added

--- server/Models/postgreSQL/database/db.cjs
+++ server/Models/postgreSQL/database/db.cjs
@@ -3,9 +3,10 @@ require("dotenv").config();
 
 // Postgres database communication
 const pool = new Pool({
+  connectionString: 'postgres://USER:PASSWORD@EXTERNAL_HOST:PORT/DATABASE',
   user: process.env.USER ?? "postgres", // my user
   password: process.env.PASSWORD ?? "jugodeub4", // my password
-  host: process.env.HOST ?? "localhost",
+  host: process.env.HOST ?? "loc alhost",
   port: process.env.PORT ?? 5432,
   database: process.env.DATABASE ?? "todoAppDatabase",
 });
