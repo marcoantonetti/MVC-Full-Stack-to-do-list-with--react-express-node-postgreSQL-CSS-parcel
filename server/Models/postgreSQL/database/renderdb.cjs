9e8cd59b12fab6a5336f8bc5364abe8d1b3a1d48
marcoantonetti <marco.a1098@gmail.com>
2024-01-10 03:42:59 -0300

    Changed port

--- server/Models/postgreSQL/database/renderdb.cjs
+++ server/Models/postgreSQL/database/renderdb.cjs
@@ -1,12 +1,26 @@
+// const Pool = require("pg").Pool;
+// require("dotenv").config();
+
+// // Postgres database communication
+// const pool = new Pool({
+//   connectionString:
+//     "postgres://todoappdatabase_user:86NegNcGMLrhyatSuN8OlnAKZ8M6fhEQ@dpg-cmbofe6d3nmc73ektfdg-a.oregon-postgres.render.com/todoappdatabase",
+//   ssl: true,
+//   ssl: { rejectUnauthorized: false }
+// });
+
+// module.exports = pool;
+
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
 // Postgres database communication
 const pool = new Pool({
-  connectionString:
-    "postgres://todoappdatabase_user:86NegNcGMLrhyatSuN8OlnAKZ8M6fhEQ@dpg-cmbofe6d3nmc73ektfdg-a.oregon-postgres.render.com/todoappdatabase",
-  ssl: true,
-  ssl: { rejectUnauthorized: false },
+  user: process.env.USER ?? "todoappdatabase_user", 
+  password: process.env.PASSWORD ?? "86NegNcGMLrhyatSuN8OlnAKZ8M6fhEQ", 
+  host: process.env.HOST ?? "dpg-cmbofe6d3nmc73ektfdg-a",
+  port: process.env.PORT ?? 5432,
+  database: process.env.DATABASE ?? "todoAppDatabase",
 });
 
 module.exports = pool;
