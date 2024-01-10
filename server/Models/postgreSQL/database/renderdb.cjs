733dc3ee769b8e20fecabbe0de8fb00170682b53
marcoantonetti <marco.a1098@gmail.com>
2024-01-10 01:28:50 -0300

    Changed ssl

--- server/Models/postgreSQL/database/renderdb.cjs
+++ server/Models/postgreSQL/database/renderdb.cjs
@@ -6,6 +6,7 @@ const pool = new Pool({
   connectionString:
     "postgres://todoappdatabase_user:86NegNcGMLrhyatSuN8OlnAKZ8M6fhEQ@dpg-cmbofe6d3nmc73ektfdg-a/todoappdatabase",
   ssl: true,
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
