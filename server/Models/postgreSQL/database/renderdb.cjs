3c1498cbe79e07a4ed0350f83ae6e464e365ffdf
marcoantonetti <marco.a1098@gmail.com>
2024-01-10 02:03:18 -0300

    Changed url for render hosting

--- server/Models/postgreSQL/database/renderdb.cjs
+++ server/Models/postgreSQL/database/renderdb.cjs
@@ -4,9 +4,9 @@ require("dotenv").config();
 // Postgres database communication
 const pool = new Pool({
   connectionString:
-    "postgres://todoappdatabase_user:86NegNcGMLrhyatSuN8OlnAKZ8M6fhEQ@dpg-cmbofe6d3nmc73ektfdg-a/todoappdatabase",
+    "postgres://todoappdatabase_user:86NegNcGMLrhyatSuN8OlnAKZ8M6fhEQ@dpg-cmbofe6d3nmc73ektfdg-a.oregon-postgres.render.com/todoappdatabase",
   ssl: true,
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
