eea3ce3bec90b0d82060b6863259d7b27ecbd553
marcoantonetti <marco.a1098@gmail.com>
2024-01-10 23:06:14 -0300

    fixed port number

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 // modules
 import express, { json } from "express"; // require -> commonJS
-import "dotenv/config";
 import { createTodoRouter } from "./Routes/todoRouter.js";
 import { createUserRouter } from "./Routes/userRouter.js";
 import cors from "cors";
@@ -8,7 +7,7 @@ import bodyParser from "body-parser";
 
 // después
 export const createApp = ({ todoModel, userModel }) => {
-  const port = 5432;
+  const port = 3000;
   const hostname = "localhost";
   const app = express();
   console.log(todoModel, { todoModel });
@@ -20,7 +19,7 @@ export const createApp = ({ todoModel, userModel }) => {
   app.use("/todos", createTodoRouter({ todoModel }));
   app.use("/user", createUserRouter({ userModel }));
 
-  app.listen(5432, () => {
-    console.log(`server listening on port ${5432}`);
+  app.listen(port, () => {
+    console.log(`server listening on port ${port}`);
   });
 };
