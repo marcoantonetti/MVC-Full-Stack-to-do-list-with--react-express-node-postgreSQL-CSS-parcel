3af82db8c86d283f85196410af4e631ec8112427
marcoantonetti <marco.a1098@gmail.com>
2024-01-10 23:23:52 -0300

    fixed port number..

--- server/Models/postgreSQL/database/renderdb.cjs
+++ server/Models/postgreSQL/database/renderdb.cjs
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 // Postgres database communication
 const pool = new Pool({
-  connectionString: process.env.URL,
+  connectionString: process.env.URL ?? "postgres://todoappdatabase_user:86NegNcGMLrhyatSuN8OlnAKZ8M6fhEQ@dpg-cmbofe6d3nmc73ektfdg-a.oregon-postgres.render.com/todoappdatabase",
   ssl: { rejectUnauthorized: false },
   ssl: true,
 });
