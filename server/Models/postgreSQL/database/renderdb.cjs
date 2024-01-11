7659aa30333f3fdd216dad178d4255b0bac04940
marcoantonetti <marco.a1098@gmail.com>
2024-01-10 22:48:24 -0300

    added env for the render url

--- server/Models/postgreSQL/database/renderdb.cjs
+++ server/Models/postgreSQL/database/renderdb.cjs
@@ -4,6 +4,7 @@ require("dotenv").config();
 // Postgres database communication
 const pool = new Pool({
   connectionString: process.env.URL,
+  ssl: { rejectUnauthorized: false },
   ssl: true,
 });
 
